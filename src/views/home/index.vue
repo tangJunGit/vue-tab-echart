6bc83492e54e694b4f9e6207484b3bbb13f1e6be
tangjun <tj_910601@163.com>
2019-04-26 10:41:18 +0800

    tab-echart组合

---
+++ src/views/home/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <el-tabs type="card" v-model="tabItem">
+      <el-tab-pane label="tab1" name="tab1">
+        <h1>tab1</h1>
+        <charts v-if="'tab1' === tabItem" :data="data">tab1</charts>
+      </el-tab-pane>
+      <el-tab-pane label="tab2" name="tab2">
+        <h1>tab2</h1>
+        <charts v-if="'tab2' === tabItem" :data="data">tab2</charts>
+      </el-tab-pane>
+      <el-tab-pane label="tab3" name="tab3">
+        <h1>tab3</h1>
+        <charts v-if="'tab3' === tabItem" :data="data">tab3</charts>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+
+<script>
+import Vue from "Vue";
+import charts from "@/components/charts";
+export default {
+  name: "index",
+  components: {
+      charts
+  },
+  data(){
+      return {
+        tabItem: "tab1",
+        data: null
+      }
+  },
+  created(){
+    // setTimeout 模拟请求数据
+    setTimeout(() => {
+      this.data = {
+        title: "ECharts 入门示例",
+        xAxis: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+        series: [5, 20, 36, 10, 10, 20]
+      }
+
+      let temp = this.tabItem;
+      this.tabItem = '';
+      Vue.nextTick(() => {
+        this.tabItem = temp;
+      })
+    }, 5000);
+
+  },
+  
+};
+</script>
+
+<style>
+</style>
